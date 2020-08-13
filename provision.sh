61cb2c724c00d68617bd6dfbdf0d6b50d4544884
Abdulrahman A <abdulrahman.asiri.a@gmail.com>
2020-08-13 11:56:41 +0300

    mounting my local dir

--- provision.sh
+++ provision.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
+
+# get the nginx working
+sudo touch /etc/systemd/system/nginx.service
+sudo chmod a+rw /etc/systemd/system/nginx.service
+sudo docker pull nginx
+
+cat <<EOF > /etc/systemd/system/nginx.service
+[Unit]
+Description=abdul nginx
+After=docker.service
+[Service]
+Type=simple
+ExecStart=sudo docker container run -d --name nginx2020 -p 80:80 --restart=always -v /home/ubuntu:/mywork nginx
+[Install]
+WantedBy=multi-user.target
+EOF
+ls -Alth
+pwd
+sudo chmod a-w /etc/systemd/system/nginx.service
+sudo systemctl daemon-reload
+sudo systemctl enable nginx
+sudo systemctl start nginx
 echo "done."
